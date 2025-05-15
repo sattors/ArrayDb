a25566fa0033886f36c231c2f791e0cb1694c3c7
sattors <sattorbekoff@gmail.com>
2025-05-15 10:13:10 +0500

    Update index.php 🧠

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ require_once __DIR__ . "/vendor/autoload.php";
 use Cache\ArrayDb;
 
 $db = new ArrayDb();
-$db->set("a", "b", "Salom");
-echo $db->get("a", "b") . PHP_EOL;
+$db->set("key", "author", "Hello");
+echo $db->get("key", "author");
